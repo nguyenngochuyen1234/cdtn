1d623815b4db1a9fe1956e0108c773ed08571918
ducbao <truongducbao290402@gmail.com>
2025-02-10 23:12:18 +0700

    fix create shop

--- src/pages/createShop/MapComponent.tsx
+++ src/pages/createShop/MapComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import 'leaflet/dist/leaflet.css';
 
 const MapComponent: React.FC = () => {
     return (
